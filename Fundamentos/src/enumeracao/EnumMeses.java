2299e133f56ea4519d8deded20c9cb171ce26e21
Maria Clara <maria.tremocoldi@aluno.senai.br>
2023-08-17 11:21:20 -0300

    Enviando enumerações

---
+++ Fundamentos/src/enumeracao/EnumMeses.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enumeracao;
+
+import com.sun.tools.javac.Main;
+
+/**
+ *
+ * @author Aluno
+ */
+public enum EnumMeses {
+    
+    JANEIRO,
+    FEVEREIRO,
+    MARCO,
+    ABRIL, 
+    MAIO,
+    JUNHO,
+    LULHO,
+    AGOSTO, 
+    SETEMBRO, 
+    OUTUBRO, 
+    NOVEMBRO, 
+    DEZEMBRO;
+   
+}
+
+
+
---
+++ Fundamentos/src/enumeracao/EnumProdutos.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enumeracao;
+
+import com.sun.jdi.connect.LaunchingConnector;
+import com.sun.source.doctree.DocCommentTree;
+
+/**
+ *
+ * @author Aluno
+ */
+public enum EnumProdutos {
+    
+    SALGADO, REFRIGERENTE, SUCO, BALA, LANCHE,SALGADINHO,PRATO, DOCE;
+}
